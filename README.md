ea356aa2e8a1bb2de2249beb643a3888dc45bc89
WILL MEIER <willmeierart@gmail.com>
2018-04-05 10:38:56 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Proxy Server for weaving together multiple graphql endpoints
+
+### because apollo-client doesn't like async introspectSchema queries at runtime
+
+just add new GraphCMS endpoints here and query the deployed proxy server
+many thanks to the people behind apollo-server and graphql-weaver
+
+developed by Will Meier at Agency Zero 2018 😎
---
+++ index.js
@@ -0,0 +1,48 @@
+import 'babel-polyfill'
+import fetch from 'node-fetch'
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { weaveSchemas } from 'graphql-weaver'
+
+if (!process.browser) global.fetch = fetch
+
+const PORT = 3010
+const app = express()
+
+const CWUE = 'https://api.graphcms.com/simple/v1/carwashusaexpress'
+const CLONE = 'https://api.graphcms.com/simple/v1/cjfjtoyj6015w0150471puqoo'
+const ALL_ENDPOINTS = [CWUE, CLONE]
+
+async function run() {
+  const schema = await weaveSchemas({
+    endpoints: [
+      {
+        namespace: '_carwashUSAexpress',
+        typePrefix: 'Primary',
+        url: CWUE
+      },
+      {
+        namespace: '_clone',
+        typePrefix: 'Secondary',
+        url: CLONE
+      }
+    ]
+  })
+
+  app.use(cors({ allow: '*' }))
+  app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+  app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+  app.post('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+
+  app.listen(PORT, () => {
+    console.log(`running on ${PORT}`)
+  })
+}
+
+try {
+  run()
+} catch (e) {
+  console.log(e, e.message, e.stack)
+}
